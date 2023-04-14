130e257d413458849930c926b47e71734319d5ac
ysk9926 <74546856+ysk9926@users.noreply.github.com>
2023-04-14 19:00:04 +0900

    230414_study_2

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import styled, { createGlobalStyle } from "styled-components";
-import { Reset } from "styled-reset";
+import { useRef } from "react";
 import { ThemeProvider } from "styled-components";
 import { darkTheme, lightTheme } from "./theme";
 import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
 import { hover } from "@testing-library/user-event/dist/hover";
+import { type } from "os";
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
@@ -83,16 +84,45 @@ const Overlay = styled(motion.div)`
   }
 `;
 
-const Btn = styled.button`
+const Btn = styled(motion.button)<{ isOn: boolean }>`
+  border: none;
   position: absolute;
-  bottom: 50px;
+  bottom: 200px;
+  border-radius: 15px;
+  padding: 5px 10px;
+  background-color: rgba(255, 255, 255, 0.6);
+  width: 70px;
+  display: flex;
+  justify-content: flex-start;
+  justify-content: ${(props) => (props.isOn ? "flex-end" : "flex-start")};
+`;
+
+const BtnHandle = styled(motion.div)`
+  width: 20px;
+  height: 20px;
+  background-color: white;
+  border-radius: 10px;
 `;
 
 const boxVar = {
-  hover: (custom: number) => ({
+  hover: (custom: string) => ({
     scale: 1.2,
-    x: custom === 1 ? -30 : custom === 2 ? 30 : custom === 3 ? -30 : 30,
-    y: custom === 1 ? -20 : custom === 2 ? -20 : custom === 3 ? 20 : 20,
+    x:
+      custom === "box1"
+        ? -30
+        : custom === "box2"
+        ? 30
+        : custom === "box3"
+        ? -30
+        : 30,
+    y:
+      custom === "box1"
+        ? -20
+        : custom === "box2"
+        ? -20
+        : custom === "box3"
+        ? 20
+        : 20,
   }),
 };
 
@@ -102,11 +132,21 @@ const OverlayVar = {
   exit: { backgroundColor: "rgba(0, 0, 0, 0)" },
 };
 
-function App() {
-  const [id, setId] = useState<null | Number>(null);
-  const [switchBtn, setSwithBtn] = useState(false);
+const switchVar = {
+  start: {},
+  end: {},
+  exit: {},
+};
 
-  const btnClick = () => setSwithBtn((pre) => !pre);
+function App() {
+  const [id, setId] = useState<null | string>(null);
+  const [switchBtn, setSwithBtn] = useState(false);
+  const [isOn, setIsOn] = useState(false);
+
+  const btnClick = () => {
+    setSwithBtn((pre) => !pre);
+    setIsOn((pre) => !pre);
+  };
   return (
     <>
       <ThemeProvider theme={darkTheme}>
@@ -116,50 +156,97 @@ function App() {
         <Wrapper>
           <GridWrapper>
             <Grid>
-              <Box
-                onClick={() => setId(1)}
-                custom={1}
-                variants={boxVar}
-                whileHover={{ dura }}
-              />
-              <Box
-                onClick={() => setId(2)}
-                custom={2}
-                variants={boxVar}
-                whileHover="hover"
-              >
-                {switchBtn ? <Circle layoutId="circle" /> : null}
-              </Box>
-              <Box
-                onClick={() => setId(3)}
-                custom={3}
-                variants={boxVar}
-                whileHover="hover"
-              >
-                {switchBtn ? null : <Circle layoutId="circle" />}
-              </Box>
-              <Box
-                custom={4}
-                onClick={() => setId(4)}
-                variants={boxVar}
-                whileHover="hover"
-              />
+              <AnimatePresence>
+                <Box
+                  onClick={() => setId("box1")}
+                  layoutId="box1"
+                  custom={"box1"}
+                  variants={boxVar}
+                  whileHover="hover"
+                  transition={{ duration: 0.3, type: "tween" }}
+                />
+                <Box
+                  onClick={() => setId("box2")}
+                  layoutId="box2"
+                  custom={"box2"}
+                  variants={boxVar}
+                  whileHover="hover"
+                  transition={{ type: "tween", duration: 0.3 }}
+                >
+                  {switchBtn ? (
+                    <Circle
+                      drag
+                      dragSnapToOrigin
+                      dragElastic={0.8}
+                      layoutId="circle"
+                      transition={{ duration: 0.5 }}
+                      variants={switchVar}
+                      initial="start"
+                      animate="end"
+                      exit="exit"
+                    />
+                  ) : null}
+                </Box>
+                <Box
+                  onClick={() => setId("box3")}
+                  layoutId="box3"
+                  custom={"box3"}
+                  variants={boxVar}
+                  whileHover="hover"
+                  transition={{ type: "tween", duration: 0.3 }}
+                >
+                  {switchBtn ? null : (
+                    <Circle
+                      drag
+                      dragSnapToOrigin
+                      dragElastic={0.8}
+                      layoutId="circle"
+                      transition={{ duration: 0.5 }}
+                      variants={switchVar}
+                      initial="start"
+                      animate="end"
+                      exit="exit"
+                    />
+                  )}
+                </Box>
+                <Box
+                  custom={"box4"}
+                  onClick={() => setId("box4")}
+                  layoutId="box4"
+                  variants={boxVar}
+                  whileHover="hover"
+                  transition={{ type: "tween", duration: 0.3 }}
+                />
+              </AnimatePresence>
             </Grid>
             <AnimatePresence>
               {id ? (
                 <Overlay
                   variants={OverlayVar}
                   onClick={() => setId(null)}
+                  transition={{ duration: 0.3 }}
                   initial="hidden"
                   animate="visible"
                   exit="exit"
                 >
-                  <Box></Box>
+                  <Box
+                    layoutId={id}
+                    transition={{
+                      duration: 0.5,
+                      ease: "easeInOut",
+                      delay: 0.1,
+                    }}
+                    initial={{ backgroundColor: "rgba(255, 255, 255, 0.4)" }}
+                    animate={{ backgroundColor: "rgba(255,255,255,1)" }}
+                    exit={{ scale: 0 }}
+                  ></Box>
                 </Overlay>
               ) : null}
             </AnimatePresence>
           </GridWrapper>
-          <Btn onClick={btnClick}>whitch</Btn>
+          <Btn onClick={btnClick} isOn={isOn}>
+            <BtnHandle layoutId="btnHandle" />
+          </Btn>
         </Wrapper>
       </ThemeProvider>
     </>

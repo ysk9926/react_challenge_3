5d1c0306bcfa75e2b08d25011d56b071fce39fd2
ysk9926 <74546856+ysk9926@users.noreply.github.com>
2023-04-14 15:35:11 +0900

    230414_study

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import styled, { createGlobalStyle } from "styled-components";
 import { Reset } from "styled-reset";
 import { ThemeProvider } from "styled-components";
 import { darkTheme, lightTheme } from "./theme";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
+import { hover } from "@testing-library/user-event/dist/hover";
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
 
-*{
-  box-sizing: border-box;
-}
+
 
 body{
   font-family: 'Source Sans Pro', sans-serif;
@@ -27,36 +27,139 @@ a{
 }
 
 `;
-// GlobalStyle을 사용해서 App안에있는 모든 컴포넌트에 요소들에게 일괄 적용
+
 const Wrapper = styled.div`
+  width: 100%;
   height: 100vh;
-  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+// GlobalStyle을 사용해서 App안에있는 모든 컴포넌트에 요소들에게 일괄 적용
+const GridWrapper = styled.div`
+  width: 100%;
+  height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
-const Box = styled(motion.div)`
-  width: 200px;
-  height: 200px;
-  background-color: white;
-  border-radius: 10px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 10px;
 `;
 
+const Box = styled(motion.div)`
+  width: 300px;
+  height: 200px;
+  background-color: rgba(255, 255, 255, 0.4);
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  border-radius: 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Circle = styled(motion.div)`
+  width: 70px;
+  height: 70px;
+  border-radius: 50%;
+  background-color: white;
+`;
+
+const Overlay = styled(motion.div)`
+  width: 100%;
+  height: 100vh;
+
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  div {
+    width: 400px;
+    height: 300px;
+    background-color: rgba(255, 255, 255, 1);
+  }
+`;
+
+const Btn = styled.button`
+  position: absolute;
+  bottom: 50px;
+`;
+
+const boxVar = {
+  hover: (custom: number) => ({
+    scale: 1.2,
+    x: custom === 1 ? -30 : custom === 2 ? 30 : custom === 3 ? -30 : 30,
+    y: custom === 1 ? -20 : custom === 2 ? -20 : custom === 3 ? 20 : 20,
+  }),
+};
+
+const OverlayVar = {
+  hidden: { backgroundColor: "rgba(0, 0, 0, 0)" },
+  visible: { backgroundColor: "rgba(0, 0, 0, 0.5)" },
+  exit: { backgroundColor: "rgba(0, 0, 0, 0)" },
+};
+
 function App() {
+  const [id, setId] = useState<null | Number>(null);
+  const [switchBtn, setSwithBtn] = useState(false);
+
+  const btnClick = () => setSwithBtn((pre) => !pre);
   return (
     <>
       <ThemeProvider theme={darkTheme}>
         <GlobalStyle />
-        <Reset />
+
         {/* Reset은 reactReset npm을 사용해서 기본적인 css를 초기화시켜준다 */}
         <Wrapper>
-          <Box
-            initial={{ scale: 0 }}
-            transition={{ delay: 1, duration: 1, bounce: 0.5, type: "spring" }}
-            animate={{ rotateZ: 360, scale: 1 }}
-          />
+          <GridWrapper>
+            <Grid>
+              <Box
+                onClick={() => setId(1)}
+                custom={1}
+                variants={boxVar}
+                whileHover={{ dura }}
+              />
+              <Box
+                onClick={() => setId(2)}
+                custom={2}
+                variants={boxVar}
+                whileHover="hover"
+              >
+                {switchBtn ? <Circle layoutId="circle" /> : null}
+              </Box>
+              <Box
+                onClick={() => setId(3)}
+                custom={3}
+                variants={boxVar}
+                whileHover="hover"
+              >
+                {switchBtn ? null : <Circle layoutId="circle" />}
+              </Box>
+              <Box
+                custom={4}
+                onClick={() => setId(4)}
+                variants={boxVar}
+                whileHover="hover"
+              />
+            </Grid>
+            <AnimatePresence>
+              {id ? (
+                <Overlay
+                  variants={OverlayVar}
+                  onClick={() => setId(null)}
+                  initial="hidden"
+                  animate="visible"
+                  exit="exit"
+                >
+                  <Box></Box>
+                </Overlay>
+              ) : null}
+            </AnimatePresence>
+          </GridWrapper>
+          <Btn onClick={btnClick}>whitch</Btn>
         </Wrapper>
       </ThemeProvider>
     </>
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom/client";
 import { RecoilRoot } from "recoil";
 
 import App from "./App";
+import { Reset } from "styled-reset";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -10,7 +11,7 @@ root.render(
   <div>
     <RecoilRoot>
       {/* RecoilRoot를 사용해서 안에있는 컴포넌트에서 state값을 편하게 공유가능 */}
-
+      <Reset />
       {/* ThemeProvider는 전체 컴포넌트에 theme을 적용할수있게 도와준다 */}
       <App />
     </RecoilRoot>
